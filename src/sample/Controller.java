794547b8d452fe4a1657b72c31991bc98669c2e1
Tomasz Miller <info@tomaszmiller.pl>
2017-06-13 11:38:35 +0200

    main controller updated

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,6 +1,40 @@
 package sample;
 
-public class Controller {
+import com.jfoenix.controls.JFXDatePicker;
+import com.jfoenix.controls.JFXListView;
+import com.jfoenix.controls.JFXTextField;
+import com.jfoenix.controls.JFXTimePicker;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    @FXML
+    private JFXTextField userName;
+
+    @FXML
+    private JFXListView<Room> rooms;
+
+    @FXML
+    private JFXDatePicker startDate;
+
+    @FXML
+    private JFXTimePicker startTime;
+
+    @FXML
+    private JFXDatePicker endDate;
+
+    @FXML
+    private JFXTimePicker endTime;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        startTime.setIs24HourView(true);
+        endTime.setIs24HourView(true);
+    }
 
 
 
