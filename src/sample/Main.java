4d6c63ca929f1a95d718b99326d9fcfb48c96609
Tomasz Miller <info@tomaszmiller.pl>
2017-06-21 11:13:27 +0200

    rename of application

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("SRS - System Rezerwacji Sal");
+        primaryStage.setTitle("Room Reservation System");
         primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.setFullScreen(false);
         primaryStage.setMaximized(false);
